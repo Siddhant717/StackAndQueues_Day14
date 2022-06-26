e6bd64fd3cd24ea2473eef46d86b9444fc408da0
Siddhant <santoshshrivastava39@gmail.com>
2022-06-26 16:42:24 +0530

    UC3 solution added

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -6,13 +6,11 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Stack st = new Stack(3);
-            st.Push(56);
-            st.Push(30);
-            st.Push(70);
-            Console.WriteLine(st.ToString());
-            Console.WriteLine("The Peek Value is " + st.Peek());
-          
+            Queue queue = new Queue(3);
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            Console.WriteLine(queue.ToString());
         }
     }
 }
