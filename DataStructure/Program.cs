15b18ed105913a9cad73f9fad0d002db24a9c8ae
Siddhant <santoshshrivastava39@gmail.com>
2022-06-26 16:36:11 +0530

    UC2 solution added

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,6 +11,8 @@ namespace DataStructure
             st.Push(30);
             st.Push(70);
             Console.WriteLine(st.ToString());
+            Console.WriteLine("The Peek Value is " + st.Peek());
+          
         }
     }
 }
