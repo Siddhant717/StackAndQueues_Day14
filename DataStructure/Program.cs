423f425bdda6bbb2bdb32d8f1b56ec43a1f4af21
Siddhant <santoshshrivastava39@gmail.com>
2022-06-26 16:52:10 +0530

    UC4 solution added

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -10,7 +10,13 @@ namespace DataStructure
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
-            Console.WriteLine(queue.ToString());
+            Console.WriteLine(queue.ToString());                                   //It display 56 30 70
+            Console.WriteLine("Dequeue method removed " + queue.Dequeue());        //It removes 56
+            Console.WriteLine(queue.ToString());                                   // It display 30 and 70
+            Console.WriteLine("Dequeue method removed " + queue.Dequeue());        // It removes 30
+            Console.WriteLine(queue.ToString());                                   // It display 70
+            Console.WriteLine("Dequeue method removed " + queue.Dequeue());        //It removes 70
+            Console.WriteLine(queue.ToString());                                   // It Display empty queue
         }
     }
 }
